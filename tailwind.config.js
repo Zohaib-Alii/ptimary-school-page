e96084032d38a49f6c21791a7c686c5aac0689da
Zohaib Arain <64296427+Zohaib-Alii@users.noreply.github.com>
2022-01-07 15:05:09 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Otomanopee One"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
